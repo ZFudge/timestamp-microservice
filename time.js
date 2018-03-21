dbc7de225b0b402212c60843aae9109cc76e02d0
Zach Fudge <zacheryfudge@gmail.com>
2018-03-21 13:14:44 -0600

    services natural date

--- time.js
+++ time.js
@@ -1,26 +1,49 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
     var obj = url.parse(req.url, true);
     var path = obj.path.replace(/\//, "");
-    var time = {
-    	"unixtime": null,
-    	"natural": null
-    };
     
-    if (!isNaN(path)) {
-		time.unixtime = Number(path);
-		time.natural = unixToNatural(Number(path))
-    } else if (valiDate(path)) {
-
-    }
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(time));
+    if (path === "") {
+    	fs.readFile('./index.html', function (err, html) {
+		    if (err) throw err; 
+	        res.writeHeader(200, {"Content-Type": "text/html"});  
+	        res.write(html);  
+	        res.end();  
+		});
+    } else {
+    	var time = { "unixtime": null, "natural": null };
+    	if (!isNaN(path)) {
+			time.unixtime = Number(path);
+			time.natural = unixToNatural(Number(path))
+	    } else if (valiDate(path)) {
+	    	var str = path.split("%20");
+	    	var date = new Date(str[2], months.indexOf(str[0]), str[1].replace(",",""), 0, 0, 0, 0);
+	    	console.log(date, str[2], months.indexOf(str[0]), str[1])
+			time.unixtime = date.getTime() / 1000;
+			time.natural = path.replace(/%20/g, " ");
+	    }
+	    res.writeHead(200, { 'Content-Type': 'application/json' });
+	    res.end(JSON.stringify(time));
+	}
 }).listen(1337, "localhost");
 
-function valiDate(date) {
-	return false;
+function valiDate(path) {
+	return months.includes(path.split("%20")[0]);
 }
 
-var unixToNatural = (n) => new Date(n*1000);
+var unixToNatural = (n) => {
+	var date = new Date(n*1000);
+	var natural = "";
+	natural += months[date.getMonth()] + " ";
+	natural += date.getDate() + ", ";
+	natural += date.getFullYear();
+	return natural;
+}
+
+var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+
